d818309bdf1fdf0666c6ea7ba10f72fd4567edb2
unknown <khedkarmahesh14@gmail.com>
2023-11-22 11:15:11 +0530

    UI finishing and mobile responsive

--- src/Pages/Company/CompanySlider.js
+++ src/Pages/Company/CompanySlider.js
@@ -1,62 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import './CompanySlider.css'
+import "./CompanySlider.css";
 
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 
 const CompanySlider = () => {
-    
-
-
-
-    
-   const [data]=useState([
+  const [data] = useState([
     {
-        imgData:'https://trfastenings.blob.core.windows.net/sitecontent/8e5698e861c94c95952f5fe9d7777a61.png',
-        head:'Global footprint',
-        para:'The TR Group of companies consists of 33&nbsp;locations within the UK, Asia, Europe and the USA including seven&nbsp;high volume sites manufacturing cold forged fasteners and special parts and three&nbsp;technical and innovation centres.'
-
+      imgData:
+        "https://trfastenings.blob.core.windows.net/sitecontent/8e5698e861c94c95952f5fe9d7777a61.png",
+      head: "Global footprint",
+      para: "The TR Group of companies consists of 33&nbsp;locations within the UK, Asia, Europe and the USA including seven&nbsp;high volume sites manufacturing cold forged fasteners and special parts and three&nbsp;technical and innovation centres.",
     },
     {
-        imgData:'https://trfastenings.blob.core.windows.net/sitecontent/4e547b11af8c4b4e8c865af05973aa08.png',
-        head:'Who we supply',
-        para:`We supply components to a mix of OEM's and Tier 1 companies globally across a wide range of industries including; light vehicle, heavy vehicle, health &amp; home, energy, tech &amp; infrastructure, general industrial and distributors.`
+      imgData:
+        "https://trfastenings.blob.core.windows.net/sitecontent/4e547b11af8c4b4e8c865af05973aa08.png",
+      head: "Who we supply",
+      para: `We supply components to a mix of OEM's and Tier 1 companies globally across a wide range of industries including; light vehicle, heavy vehicle, health &amp; home, energy, tech &amp; infrastructure, general industrial and distributors.`,
     },
     {
-        imgData:'https://trfastenings.blob.core.windows.net/sitecontent/a9412ef56ce14112986cf01a8a9c39fa.png',
-        head:'Our Services',
-        para:'We deliver comprehensive support to our customers across every requirement, from concept design through to technical engineering consultancy, manufacturing, supply management and global logistics.'
-    }
-   ])
+      imgData:
+        "https://trfastenings.blob.core.windows.net/sitecontent/a9412ef56ce14112986cf01a8a9c39fa.png",
+      head: "Our Services",
+      para: "We deliver comprehensive support to our customers across every requirement, from concept design through to technical engineering consultancy, manufacturing, supply management and global logistics.",
+    },
+  ]);
   return (
-    <div style={{marginTop:'20px'}}>
-       <Carousel  >
-      {
-        data && data.map((e)=>{
-            return(
-                <Carousel.Item className='Company-carousal'> 
+    <div style={{ marginTop: "20px" }}>
+      <Carousel>
+        {data &&
+          data.map((e) => {
+            return (
+              <Carousel.Item className="Company-carousal">
                 {/* <img src={e.imgData} height='100%' width='100%'></img> */}
-           <div className='Company-slider-main-div'>
-            <img src={e.imgData}></img>
-            <div className='Company-slider-inner-div'>
-                 <div>
-                    <h4>{e.head}</h4>
-                   
-                    <p>{e.para}</p>
-                    <button>Find out more</button>
-                 </div>
-            </div>
-        </div>
-           
-                </Carousel.Item>
-            )
-        })
-      }
- 
-  
-    </Carousel>
-    </div>
-  )
-}
+                <div className="Company-slider-main-div">
+                  <img src={e.imgData}></img>
+                  <div className="Company-slider-inner-div">
+                    <div>
+                      <h4>{e.head}</h4>
 
-export default CompanySlider
+                      <p>{e.para}</p>
+                      <button>Find out more</button>
+                    </div>
+                  </div>
+                </div>
+              </Carousel.Item>
+            );
+          })}
+      </Carousel>
+    </div>
+  );
+};
+
+export default CompanySlider;
--- src/Pages/Landing/Carorsel.js
+++ src/Pages/Landing/Carorsel.js
@@ -1,35 +1,109 @@
 import Carousel from 'react-bootstrap/Carousel';
 // import ExampleCarouselImage from 'components/ExampleCarouselImage';
 import './Carousel.css';
+import { useNavigate } from 'react-router';
 
 function UncontrolledExample() {
+
+  const navigate = useNavigate();
+
   return (
     <Carousel>
       <Carousel.Item className='carouselItem'>
         <img src='https://trfastenings.blob.core.windows.net/sitecontent/48d2fefe879c42b79c75d2ad12f6bb23.jpg'/>
-        {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
+        <Carousel.Caption className='Carousel-Caption'>
+        <div className='Carousel-Caption-Div'>
+            <h3 style={{color:'black'}}>Product visualisers</h3>
+            <p>TR’s Product Visualisers assist companies at the design stage. A range of options enables the user to interact with the products.</p>
+            <button onClick={()=> navigate('./products')}>
+              Find out more
+            </button>
+          </div>
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className='carouselItem'>
         <img src='https://trfastenings.blob.core.windows.net/sitecontent/332bead8d69449bd8c325afa9264f225.jpg'/>
-        {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
+        <Carousel.Caption className='Carousel-Caption'>
+          <div className='Carousel-Caption-Div'>
+            <h3 style={{color:'black'}}>Knowledge Base</h3>
+            <p>Find engineering data on our range of products including installation and tooling guidelines, mechanical and material properties and more.</p>
+            <button onClick={()=> navigate('./knowledge-base')}>
+              Find out more
+            </button>
+          </div>
+        </Carousel.Caption> 
       </Carousel.Item>
       <Carousel.Item className='carouselItem'>
         <img src='https://trfastenings.blob.core.windows.net/sitecontent/5642de1a9dd54689b65fe418ceabe9ac.png'/>
-        {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption> */}
+        <Carousel.Caption className='Carousel-Caption'>
+        <div className='Carousel-Caption-Div'>
+            <h3 style={{color:'black'}}>TR in industry</h3>
+            <p>Blending our global manufacturing capability with strong distribution and logistic support creates a winning combination.</p>
+            <button onClick={()=> navigate('./industries')}>
+              Find out more
+            </button>
+          </div>
+        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+    
   );
 }
 
 export default UncontrolledExample;
+
+
+// import React, { useState } from "react";
+// import "./Carousel.css";
+
+// import Carousel from "react-bootstrap/Carousel";
+
+// const CompanySlider = () => {
+//   const [data] = useState([
+//     {
+//       imgData:
+//         "https://trfastenings.blob.core.windows.net/sitecontent/48d2fefe879c42b79c75d2ad12f6bb23.jpg",
+//       head: "Product visualisers",
+//       para: "TR’s Product Visualisers assist companies at the design stage. A range of options enables the user to interact with the products.",
+//     },
+//     {
+//       imgData:
+//         "https://trfastenings.blob.core.windows.net/sitecontent/332bead8d69449bd8c325afa9264f225.jpg",
+//       head: "Knowledge Base",
+//       para: `Find engineering data on our range of products including installation and tooling guidelines, mechanical and material properties and more.`,
+//     },
+//     {
+//       imgData:
+//         "https://trfastenings.blob.core.windows.net/sitecontent/5642de1a9dd54689b65fe418ceabe9ac.png",
+//       head: "TR in industry",
+//       para: "Blending our global manufacturing capability with strong distribution and logistic support creates a winning combination.",
+//     },
+//   ]);
+//   return (
+//     <div style={{ marginTop: "20px" }}>
+//       <Carousel>
+//         {data &&
+//           data.map((e) => {
+//             return (
+//               <Carousel.Item className="Landing-carousal">
+//                 {/* <img src={e.imgData} height='100%' width='100%'></img> */}
+//                 <div className="Landing-slider-main-div">
+//                   <img src={e.imgData}></img>
+//                   <div className="Landing-slider-inner-div">
+//                     <div>
+//                       <h4>{e.head}</h4>
+
+//                       <p>{e.para}</p>
+//                       <button>Find out more</button>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </Carousel.Item>
+//             );
+//           })}
+//       </Carousel>
+//     </div>
+//   );
+// };
+
+// export default CompanySlider;
